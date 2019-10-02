950ef39bbcaae174f012e7e2afd323cb26e2df2b
Danish <danish5095@gmail.com>
2019-10-03 00:05:51 +0530

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,12 @@
-// importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-messaging.js');
-//
-// firebase.initializeApp({
-//     messagingSenderId: "999079269178",
-// });
-//
-// const messaging = firebase.messaging();
-//
+importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: "999079269178",
+});
+
+const messaging = firebase.messaging();
+
 // messaging.setBackgroundMessageHandler(function (payload) {
 //     console.log('[firebase-messaging-sw.js] Received background message ', payload);
 //     const notificationTitle = 'Background Message Title';
