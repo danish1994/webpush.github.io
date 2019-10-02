2e7b4f372565f2e14baaf6e5eb0968063fcc9d2c
Danish <danish5095@gmail.com>
2019-10-02 23:58:23 +0530

    service worked disabled

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,20 @@
-importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: "999079269178",
-});
-
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function (payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+// importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/7.0.0/firebase-messaging.js');
+//
+// firebase.initializeApp({
+//     messagingSenderId: "999079269178",
+// });
+//
+// const messaging = firebase.messaging();
+//
+// messaging.setBackgroundMessageHandler(function (payload) {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body: 'Image',
+//         icon: 'https://cdn.carbuzz.com/gallery-images/original/249000/700/249779.jpg'
+//     };
+//
+//     return self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
